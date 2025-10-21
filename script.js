79125a388f076a8d3f4a05c7fad0adf0e800efac
Dark Soul <maheshh.kumar1508.com>
2025-10-21 14:56:13 +0530

    completed designing/painting the obstacles on to the canvas

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 // Constants
 const obstacles = [];
-const rows = 5;
+const rows = 9;
 const WIDTH = 500;
 const HEIGHT = 500;
-const obstacleSpacing = 30;
+const obstacleSpacing = 38;
 const obstacleRadius = 4;
+const DECIMAL_MULTIPLIER = 1000;
 
 // get canvas and create instance
-const canvas = document.getElementById(canvas);
+const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+
+//  padding & unpadding
+function pad(n) {
+    return n * DECIMAL_MULTIPLIER;
+}
+function unpad(n) {
+    return Math.floor(n / DECIMAL_MULTIPLIER);
+}
 
 // Creating obstacles
 for (let row = 2; row <= rows; row++) {
     const numOfObstacles = row + 1;
     // y & x pos
-    const y = row * 30;
+    const y = row * 40;
     for (let col = 0; col < numOfObstacles; col++) {
-        const x = WIDTH / 2 - obstacleSpacing * (row / 2 + col);
-        obstacles.push({ x, y, radius: obstacleRadius });
+        const x = WIDTH / 2 - obstacleSpacing * (row / 2 - col);
+        obstacles.push({ x: pad(x), y: pad(y), radius: obstacleRadius });
     }
 }
 
@@ -26,7 +37,7 @@ function drawObstacles() {
     ctx.fillStyle = 'black';
     obstacles.forEach((obs) => {
         ctx.beginPath();
-        ctx.arc(obs.x, obs.y, obs.radius, 0, Math.PI * 2);
+        ctx.arc(unpad(obs.x), unpad(obs.y), obs.radius, 0, Math.PI * 2);
         ctx.fill();
         ctx.closePath();
     })
@@ -40,7 +51,6 @@ function draw() {
 
 function main() {
     draw();
-    requestAnimationFrame(update);
+    requestAnimationFrame(main);
 }
-
 main();
